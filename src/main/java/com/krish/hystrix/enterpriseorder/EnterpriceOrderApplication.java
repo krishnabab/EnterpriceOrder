ec8b45a417412fb28cebcdc804925ec5f7ce352d
Ghanta, Krishnababu (kg6378) <kg6378@att.com>
2018-02-25 02:24:59 -0500

    first commit

---
+++ src/main/java/com/krish/hystrix/enterpriseorder/EnterpriceOrderApplication.java
@@ -0,0 +1,12 @@
+package com.krish.hystrix.enterpriseorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriceOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriceOrderApplication.class, args);
+	}
+}
